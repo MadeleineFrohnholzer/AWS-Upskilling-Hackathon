8ff9cb0f8c8b255845b6e60b297e3c4822ae5b5f
nicolas.schmid <nicolas.schmid@accenture.com>
2026-07-30 08:37:19 +0200

    add digest lambda to new root module

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,20 @@ module "storage" {
   environment  = var.environment
 }
 
+# -----------------------------------------------------------------------------
+# Weekly Digest Lambda
+# -----------------------------------------------------------------------------
+module "digest_lambda" {
+  source = "../modules/digest-lambda"
+
+  audit_table_name = module.storage.upload_audit_table_name
+  audit_table_arn  = module.storage.upload_audit_table_arn
+  sender_email     = "nicolas.schmid@accenture.com"
+
+  project_name = var.project_name
+  environment  = var.environment
+}
+
 # module "presigned_url_lambda" {
 #   source = "./modules/presigned-url-lambda"
 
